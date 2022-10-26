306c5672a2f4b85e117627c9b8381d2bdcef3e43
FraNoceti <francescoluiginoceti@gmail.com>
2022-10-26 15:36:57 +0200

    lil

--- src/app/services/wallet.service.ts
+++ src/app/services/wallet.service.ts
@@ -12,18 +12,18 @@ export class WalletService {
 
   public connectWallet = async () => {
     try {
-      if (!this.ethereum) return alert('Please install meta mask');
+      if (!this.ethereum) return alert('Please install Metamask or connect through Desktop');
       const accounts = await this.ethereum.request({
         method: 'eth_requestAccounts',
       });
     } catch (e) {
-      throw new Error('No thereum object found');
+      throw new Error('No ethereum object found');
     }
   };
 
   public checkWalletConnected = async () => {
     try {
-      if (!this.ethereum) return alert('Please install meta mask ');
+      if (!this.ethereum) return alert('Please install Metamask or connect through Desktop');
       const accounts = await this.ethereum.request({ method: 'eth_accounts' });
       return accounts;
     } catch (e) {
