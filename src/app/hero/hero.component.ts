46a70b915058bd7a42c733bfd6d4720cd325ffa7
FraNoceti <francescoluiginoceti@gmail.com>
2022-10-26 15:30:41 +0200

    everything

---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { WalletService } from '../services/wallet.service';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss'],
+})
+export class HeroComponent implements OnInit {
+  title = 'ng-connect-ethereum-wallet';
+
+  public walletConnected: boolean = false;
+  public walletId: string = '';
+
+  constructor(private walletService: WalletService) {}
+
+  connectToWallet = () => {
+    this.walletService.connectWallet();
+  };
+
+  checkWalletConnected = async () => {
+    const accounts = await this.walletService.checkWalletConnected();
+    if (accounts.length > 0) {
+      this.walletConnected = true;
+      this.walletId = accounts[0];
+    }
+  };
+
+  ngOnInit(): void {
+    this.checkWalletConnected();
+  }
+}
